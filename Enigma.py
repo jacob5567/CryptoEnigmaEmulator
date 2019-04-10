974293c8de42bca8c097d1fb72b2ed6211941402
Jacob Faulk <jacob5567@gmail.com>
2019-04-10 01:19:54 -0700

    added rotor rotation and printing

--- Enigma.py
+++ Enigma.py
@@ -22,19 +22,45 @@ def main():
     print("The Enigma would convert your first letter, " + plaintext[0] + ", to " + plugboard[plaintext[0]] + ".")
 
     # Note that the print statements will be expanded on in the future, for this rough draft, I will just work on the internals of the Enigma
-    rotor_1 = dict(zip(list("HLKEGUYWRDCNTBFVQIZPMXSAJO")), list("GWHDISFZYJATEPCLVUNXMQRKOB"))
-    rotor_2 = dict(zip(list("XRUPTZEFDSHCMNOJQWLYVGIABK")), list("OPTNYFGUERBQSZWAHJMVLKDXIC"))
-    rotor_3 = dict(zip(list("THXIJYKMZDAOWVSEQFBPUNRGCL")), list("VYCKSURPTLNQBMJHDEFXOWGZAI"))
+    rotor_1 = dict(zip(list("HLKEGUYWRDCNTBFVQIZPMXSAJO"), list("GWHDISFZYJATEPCLVUNXMQRKOB")))
+    rotor_2 = dict(zip(list("XRUPTZEFDSHCMNOJQWLYVGIABK"), list("OPTNYFGUERBQSZWAHJMVLKDXIC")))
+    rotor_3 = dict(zip(list("THXIJYKMZDAOWVSEQFBPUNRGCL"), list("VYCKSURPTLNQBMJHDEFXOWGZAI")))
 
-    for character in plaintext:
-        current = character
-        print("Current character: " + current)
-        current = plugboard[current]
-        print("Turned into " + current + " by the plugboard.")
-        current = rotor_1[current]
+    print_rotor(rotor_1)
+    rotor_1 = rotate_rotor(rotor_1)
+    print_rotor(rotor_1)
+    # for character in plaintext:
+    #     current = character
+    #     print("Current character: " + current)
+    #     current = plugboard[current]
+    #     print("Turned into " + current + " by the plugboard.")
+    #     current = rotor_1[current]
         
         
 
+def rotate_rotor(rotor):
+    keys = list()
+    values = list()
+    for key, value in rotor.items():
+        keys.append(key)
+        values.append(value)
+    
+    # rotating the rotor
+    first_char = values[0]
+    for i in range(len(values) - 1):
+        values[i] = values[i + 1]
+    values[len(values) - 1] = first_char
+
+    return dict(zip(keys, values))
+
+
+def print_rotor(rotor):
+    for key in rotor.keys():
+        print(key + ' ', end='')
+    print()
+    for value in rotor.values():
+        print(value + ' ', end='')
+    print()
 
 if __name__ == "__main__":
     main()
