a45570a555f72c2efc1aa146530350bf38fb14e2
Jacob Faulk <jacob5567@gmail.com>
2019-04-10 01:00:29 -0700

    Starter file with plugboard implemented

---
+++ Enigma.py
@@ -0,0 +1,28 @@
+# Jacob Faulk
+
+
+def main():
+    plaintext = "hello there"#input("Welcome to the Enigma Machine emulator. Enter plaintext to be encrypted.\n")
+    plaintext = plaintext.upper()
+    print(plaintext)
+    print("The first step in encoding a message in the Enigma involves a plugboard.")
+    print("This would be set every day and sent out to the German soldiers in WWII.")
+    print("For this example, the plugboard will be set as follows:")
+    alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    plugboard_locations = list("ETWAYDSVFPRCQJXOGUHZNLIMBK")
+    plugboard = dict(zip(alphabet, plugboard_locations))
+    for key in plugboard.keys():
+        print(key + ' ', end='')
+    print()
+    for value in plugboard.values():
+        print(value + ' ', end='')
+    print()
+    print("In this example, " + alphabet[0] + " corresponds to " + plugboard_locations[0] + ", " + alphabet[1] + " corresponds to " + plugboard_locations[1] + ", and so on.")
+
+    print("The Enigma would convert your first letter, " + plaintext[0] + ", to " + plugboard[plaintext[0]] + ".")
+
+    # Note that the print statements will be expanded on in the future, for this rough draft, I will just work on the internals of the Enigma
+
+
+if __name__ == "__main__":
+    main()
