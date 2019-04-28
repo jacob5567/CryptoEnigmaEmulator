b33c24ee84f604b90fd9549129eea4ea05d614b2
Jacob Faulk <jacob5567@gmail.com>
2019-04-28 16:03:34 -0700

    added Rotor class and made Enigma.py use Rotor.py

--- Enigma.py
+++ Enigma.py
@@ -1,5 +1,7 @@
 # Jacob Faulk
 
+from Rotor import Rotor
+
 
 def main():
     # plaintext = input("Welcome to the Enigma Machine emulator. Enter plaintext to be encrypted.\n")
@@ -16,29 +18,31 @@ def main():
     alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
     plugboard_locations = list("ETWAYDSVFPRCQJXOGUHZNLIMBK")
     plugboard = dict(zip(alphabet, plugboard_locations))
-    
-    print_rotor(plugboard)
 
-    print("In this example, " + alphabet[0] + " corresponds to " + plugboard_locations[0] +
-          ", " + alphabet[1] + " corresponds to " + plugboard_locations[1] + ", and so on.\n")
+    plugboard = Rotor(alphabet, plugboard_locations)
+
+    print(plugboard)
+
+    print("In this example, " + plugboard.side1[0] + " corresponds to " + plugboard.side2[0] +
+          ", " + plugboard.side1[1] + " corresponds to " + plugboard.side2[1] + ", and so on.\n")
 
     print("The Enigma would convert your first letter, " +
           plaintext[0] + ", to " + plugboard[plaintext[0]] + ".\n\n")
 
     # Note that the print statements will be expanded on in the future, for this rough draft, I will just work on the internals of the Enigma
-    rotor_1 = dict(zip(list("HLKEGUYWRDCNTBFVQIZPMXSAJO"),
-                       list("GWHDISFZYJATEPCLVUNXMQRKOB")))
-    rotor_2 = dict(zip(list("XRUPTZEFDSHCMNOJQWLYVGIABK"),
-                       list("OPTNYFGUERBQSZWAHJMVLKDXIC")))
-    rotor_3 = dict(zip(list("THXIJYKMZDAOWVSEQFBPUNRGCL"),
-                       list("VYCKSURPTLNQBMJHDEFXOWGZAI")))
+    rotor_1 = Rotor(list("HLKEGUYWRDCNTBFVQIZPMXSAJO"),
+                       list("GWHDISFZYJATEPCLVUNXMQRKOB"))
+    rotor_2 = Rotor(list("XRUPTZEFDSHCMNOJQWLYVGIABK"),
+                       list("OPTNYFGUERBQSZWAHJMVLKDXIC"))
+    rotor_3 = Rotor(list("THXIJYKMZDAOWVSEQFBPUNRGCL"),
+                       list("VYCKSURPTLNQBMJHDEFXOWGZAI"))
 
     rotor_1_backup = rotor_1.copy()
     rotor_2_backup = rotor_2.copy()
     rotor_3_backup = rotor_3.copy()
 
-    reflector = dict(zip(list("ZEFUHBDMNIJGACVTQRWYXOSLPK"),
-                         list("GBURKVPSFHJTMXAIEZLNWODQYC")))
+    reflector = Rotor(list("ZEFUHBDMNIJGACVTQRWYXOSLPK"),
+                         list("GBURKVPSFHJTMXAIEZLNWODQYC"))
 
     final_result = ""
 
@@ -50,15 +54,15 @@ def main():
             print("Turned into " + current + " by the plugboard.")
             current = rotor_1[current]
             print("Turned into " + current + " by the first rotor.")
-            rotor_1 = rotate_rotor(rotor_1)
+            rotor_1.rotate()
             print("First rotor rotated.")
 
             # this rotates the other rotors when the first one makes a complete rotation
             if rotor_1 == rotor_1_backup:
-                rotor_2 = rotate_rotor(rotor_2)
+                rotor_2.rotate()
                 print("Second rotor rotated.")
                 if rotor_2 == rotor_2_backup:
-                    rotor_3 = rotate_rotor(rotor_3)
+                    rotor_3.rotate()
                     print("Third rotor rotated.")
 
             current = rotor_2[current]
@@ -77,13 +81,13 @@ def main():
             print("Turned into " + current + " by the second rotor.")
             current = rotor_1[current]
             print("Turned into " + current + " by the first rotor.")
-            rotor_1 = rotate_rotor(rotor_1)
-            print("First rotor rotated.") # TODO see if this is accurate
+            rotor_1.rotate()
+            print("First rotor rotated.")  # TODO see if this is accurate
             if rotor_1 == rotor_1_backup:
-                rotor_2 = rotate_rotor(rotor_2)
+                rotor_2.rotate()
                 print("Second rotor rotated.")
                 if rotor_2 == rotor_2_backup:
-                    rotor_3 = rotate_rotor(rotor_3)
+                    rotor_3.rotate()
                     print("Third rotor rotated.")
 
             # TODO make reverse version of the plugboard
@@ -94,35 +98,34 @@ def main():
         else:
             final_result += character
 
-    
     print("The final result is: " + final_result)
     # TODO decryption
     # TODO final presentation of the ciphertext
 
 
-def rotate_rotor(rotor):
-    keys = list()
-    values = list()
-    for key, value in rotor.items():
-        keys.append(key)
-        values.append(value)
+# def rotate_rotor(rotor):
+#     keys = list()
+#     values = list()
+#     for key, value in rotor.items():
+#         keys.append(key)
+#         values.append(value)
 
-    # rotating the rotor
-    first_char = values[0]
-    for i in range(len(values) - 1):
-        values[i] = values[i + 1]
-    values[len(values) - 1] = first_char
+#     # rotating the rotor
+#     first_char = values[0]
+#     for i in range(len(values) - 1):
+#         values[i] = values[i + 1]
+#     values[len(values) - 1] = first_char
 
-    return dict(zip(keys, values))
+#     return dict(zip(keys, values))
 
 
-def print_rotor(rotor):
-    for key in rotor.keys():
-        print(key + ' ', end='')
-    print()
-    for value in rotor.values():
-        print(value + ' ', end='')
-    print()
+# def print_rotor(rotor):
+#     for key in rotor.keys():
+#         print(key + ' ', end='')
+#     print()
+#     for value in rotor.values():
+#         print(value + ' ', end='')
+#     print()
 
 
 if __name__ == "__main__":
---
+++ Rotor.py
@@ -0,0 +1,31 @@
+# Jacob Faulk
+
+class Rotor:
+    def __init__(self, side1, side2):
+        self.side1 = side1
+        self.side2 = side2
+
+    def __getitem__(self, key):
+        key_index = self.side1.index(key)
+        return self.side2[key_index]
+
+    def rotate(self):
+        first_char = self.side2[0]
+        for i in range(len(self.side2) - 1):
+            self.side2[i] = self.side2[i + 1]
+        self.side2[len(self.side2) - 1] = first_char
+
+    def copy(self):
+        return Rotor(self.side1, self.side2)
+
+    def __eq__(self, value):
+        return self.side1 == value.side1 and self.side2 == value.side2
+
+    def __str__(self):
+        print_this = ""
+        for i in self.side1:
+            print_this += i + ' '
+        print_this += "\n"
+        for i in self.side2:
+            print_this += i + ' '
+        return print_this
